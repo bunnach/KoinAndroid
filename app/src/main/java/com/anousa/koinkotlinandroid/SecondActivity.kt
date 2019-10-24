428ce875d05836ac49d9e675c2d3f3b7c4d3ddf7
Choeung Bunna <choeung.bunna@ababank.com>
2019-10-24 09:21:19 +0700

    Koin: fix wrong inject viewmodel

--- app/src/main/java/com/anousa/koinkotlinandroid/SecondActivity.kt
+++ app/src/main/java/com/anousa/koinkotlinandroid/SecondActivity.kt
@@ -4,16 +4,18 @@ import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.anousa.koinkotlinandroid.viewmodel.MyViewModel
-import org.koin.android.ext.android.inject
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class SecondActivity : AppCompatActivity() {
 
-    private val myViewModel: MyViewModel by inject()
+    private val myViewModel: MyViewModel by viewModel()
+    private val myViewModel1: MyViewModel by viewModel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_second)
 
         Toast.makeText(this, myViewModel.getName(), Toast.LENGTH_LONG).show()
+        Toast.makeText(this, "Bunna ${myViewModel1.getName()}", Toast.LENGTH_LONG).show()
     }
 }
